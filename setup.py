1fa0df926486082e962b928fdfddf24059f3a33a
Angela Crabtree <angela.crabtree.88@gmail.com>
2023-11-30 17:24:07 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='slide_prepper',
-    version='0.1.0',    
+    version='1.0',    
     description='Preprocessing tools for bright-field Whole Slide Images (WSIs), such as H&E-stained slides',
-    url='https://github.com/shuds13/pyexample',
+    url='https://github.com/amcrabtree/preprocess-bf-wsi',
     author='Angela Crabtree',
     author_email='angela.crabtree.88@gmail.com',
     license='None',
@@ -12,12 +12,4 @@ setup(
     install_requires=['opencv-python',
                       'numpy',                     
                       ],
-
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: None',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 3',
-    ],
 )
